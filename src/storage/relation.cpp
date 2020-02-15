321db2cf02f775e1db2c08309831e7f7cad1ad33
ljy <ljy@localhost.localdomain>
2020-02-15 23:53:27 +0800

    introduce sys table scan

--- src/storage/relation.cpp
+++ src/storage/relation.cpp
@@ -9,13 +9,18 @@
 
 namespace db {
 
+static TuplePtr RowidScanTableGetNext(TableScanDescPtr scan);
+static void RowidScanTableBeginScan(TableScanDescPtr desc);
+
 class TableScanDesc {
  public:
   TableScanDesc()
       : cursor(nullptr),
         scanKey(nullptr),
         nkeys(0),
-        totalFetch(0) {
+        totalFetch(0),
+        rowidScan(false),
+        finished(false) {
 
   }
 
@@ -25,11 +30,14 @@ class TableScanDesc {
     }
   }
 
-  RelationPtr rel;
-  Cursor* cursor;
-  ScanKey* scanKey;
-  int nkeys;
-  u64 totalFetch;
+  RelationPtr rel;    /* the relation of table */
+  Cursor* cursor;     /* the scan cursor */
+  ScanKey* scanKey;   /* scan key */
+  int nkeys;          /* number of scan key */
+  u64 totalFetch;     /* total number of tuples have been fetched*/
+  bool rowidScan;     /* 可以使用rowid使用索引扫描 ? */
+  bool finished;      /* 扫描结束 */
+  TuplePtr lastTuple;
 };
 
 RelationPtr Relation::Create(TransactionPtr tran, u64 id) {
@@ -128,21 +136,77 @@ i64 Relation::TableAppend(const Tuple& tuple) {
   return id;
 }
 
+void RowidScanTableBeginScan(TableScanDescPtr desc) {
+  assert(desc->scanKey->type == INT8OID);
+  assert(desc->nkeys == 1);
+
+  desc->rowidScan = true;
+  ScanKey* scanKey = desc->scanKey;
+
+  Slice value;
+  i64 rowid = *(i64*) scanKey->argument.data();
+
+  if (scanKey->strategy == BTEqualStrategyNumber) {
+    desc->finished = true;
+
+    if (desc->rel->kvstore->Get(scanKey->argument, value)) {
+      desc->lastTuple = std::make_shared<Tuple>((char*) value.data(), value.size());
+      desc->lastTuple->SetRowID(rowid);
+    }
+  } else if (scanKey->strategy == BTGreaterEqualStrategyNumber || scanKey->strategy == BTGreaterStrategyNumber) {
+    desc->cursor = desc->rel->kvstore->Open();
+    //Position at first key greater than or equal to specified key
+    Slice key = scanKey->argument;
+    bool ok = desc->cursor->Get(key, value, MDB_SET_RANGE);
+    if (!ok) {
+      desc->finished = true;
+      return;
+    }
+
+    if (ScanKey::CheckSatisfy(scanKey, key)) {
+      desc->lastTuple = std::make_shared<Tuple>((char*) value.data(), value.size());
+      desc->lastTuple->SetRowID(rowid);
+    }
+  } else {
+    desc->cursor = desc->rel->kvstore->Open();
+  }
+}
+
 TableScanDescPtr TableBeginScan(RelationPtr rel, ScanKey* scanKey, int nkeys) {
   assert(rel->rd_rel.relkind == RELKIND_RELATION);
   TableScanDescPtr desc(new TableScanDesc());
   desc->rel = rel;
-  desc->cursor = rel->kvstore->Open();
   if (scanKey && nkeys) {
     desc->nkeys = nkeys;
     size_t bytes = sizeof(ScanKey) * nkeys;
     desc->scanKey = (ScanKey*) malloc(bytes);
     memcpy(desc->scanKey, scanKey, bytes);
   }
+
+  if (nkeys == 1 && scanKey->attno == 0) {
+    RowidScanTableBeginScan(desc);
+  } else {
+    desc->cursor = rel->kvstore->Open();
+  }
   return desc;
 }
 
 TuplePtr TableGetNext(TableScanDescPtr scan) {
+  if (scan->lastTuple) {
+    TuplePtr tuple = scan->lastTuple;
+    scan->lastTuple = nullptr;
+    return tuple;
+  }
+
+  if (scan->finished) {
+    return nullptr;
+  }
+
+  if (scan->rowidScan) {
+    return RowidScanTableGetNext(scan);
+  }
+
+  //seq scan
   assert(scan->rel->rd_rel.relkind == RELKIND_RELATION);
   Slice key;
   Slice value;
@@ -157,14 +221,14 @@ TuplePtr TableGetNext(TableScanDescPtr scan) {
       tuple.SetRowID(rowID);
 
       if (scanKey->attno == 0 && scanKey->type != INT8OID) {
-          elog(ERROR, "invalid key type %d", scanKey->type);
+        elog(ERROR, "invalid key type %d", scanKey->type);
       }
 
       Slice column;
       TupleMeta entry;
       tuple.GetTupleMeta(scanKey->attno, entry);
       if (entry.type != scanKey->type) {
-          elog(ERROR, "type not matched %d, %d", entry.type, scanKey->type);
+        elog(ERROR, "type not matched %d, %d", entry.type, scanKey->type);
       }
       column.assign(entry.data, entry.size);
 
@@ -189,9 +253,68 @@ TuplePtr TableGetNext(TableScanDescPtr scan) {
   return nullptr;
 }
 
+TuplePtr RowidScanTableGetNext(TableScanDescPtr scan) {
+  TuplePtr tuple;
+  Slice key;
+  Slice value;
+  assert(scan->scanKey->attno == 0 && scan->nkeys == 1);
+  i64 rowid = 0;
+
+  switch (scan->scanKey->strategy) {
+    case BTLessStrategyNumber:
+    case BTLessEqualStrategyNumber: {
+      TuplePtr tup;
+      bool satisfy;
+      bool ok = scan->cursor->Get(key, value, MDB_NEXT);
+
+      if (!ok) {
+        scan->finished = true;
+        break;
+      }
+      rowid = *(i64*) key.data();
+      tup = std::make_shared<Tuple>((char*) value.data(), value.size());
+
+      satisfy = ScanKey::CheckSatisfy(scan->scanKey, key);
+      if (!satisfy) {
+        scan->finished = true;
+        break;
+      }
+
+      tup->SetRowID(rowid);
+
+      if (scan->scanKey->strategy == BTEqualStrategyNumber) {
+        scan->finished = true;
+      }
+      tuple = tup;
+      break;
+    }
+
+    case BTGreaterEqualStrategyNumber:
+    case BTGreaterStrategyNumber: {
+      bool ok = scan->cursor->Get(key, value, MDB_NEXT);
+      if (!ok) {
+        scan->finished = true;
+        break;
+      }
+
+      tuple = std::make_shared<Tuple>((char*) value.data(), value.size());
+      tuple->SetRowID(*(i64*) key.data());
+      break;
+    }
+    default: {
+      elog(ERROR, "invalid strategy %d", scan->scanKey->strategy);
+      break;
+    }
+  }
+
+  return tuple;
+}
+
 void TableEndScan(TableScanDescPtr& scan) {
   assert(scan->rel->rd_rel.relkind == RELKIND_RELATION);
-  scan->rel->kvstore->Close(scan->cursor);
+  if (scan->cursor) {
+    scan->rel->kvstore->Close(scan->cursor);
+  }
   scan = nullptr;
 }
 
@@ -209,6 +332,11 @@ SysScanDescPtr SysTableBeginScan(TransactionPtr txn,
                                  ScanKey* scanKey, int nkeys) {
   SysScanDescPtr scan(new SysScanDesc());
   scan->tableRel = tableRel;
+
+  if (nkeys == 1 && scanKey->attno == 0) {
+    scan->relScan = TableBeginScan(tableRel, scanKey, nkeys);
+    return scan;
+  }
 
   if (indexId) {
     scan->indexRel = Relation::OpenIndex(txn, indexId);
@@ -241,8 +369,6 @@ SysScanDescPtr SysTableBeginScan(TransactionPtr txn,
     }
 
     scan->indexScan = IndexBeginScan(tableRel, scan->indexRel, indexKeys.data(), indexKeys.size());
-  } else {
-    scan->relScan = TableBeginScan(tableRel, scanKey, nkeys);
   }
   return scan;
 }
